49d2298eb3ded85ebe3e541e15134f54c2105d45
Alex Newell-Powers <newpowalex@gmail.com>
2022-12-10 12:51:06 -0500

    Node and PHP files (mainly node)

---
+++ PHP/demo.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Demo Form</title>
+
+  </head>
+
+  <body>
+
+    <div >
+      <h1>Demo</h1>
+      <form action="" method="post">
+
+        <label>Word:</label>
+        <input type="text" name="demo_word" id="demo_word" required=
+"required" placeholder="Please Enter Word"/><br /><br />
+
+        <input type="submit" value=" Submit " name="submit" id="submit"/><br />
+
+      </form>
+
+    </div>
+
+
+
+  </body>
+</html>
+
